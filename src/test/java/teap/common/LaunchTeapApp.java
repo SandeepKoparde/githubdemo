6cda38620193b805497483def0f7d0d463778b95
SandeepKoparde <sandykop@gmail.com>
2019-12-09 14:55:09 +0530

    Added DemoTest case for Demo App

--- src/test/java/teap/common/LaunchTeapApp.java
+++ src/test/java/teap/common/LaunchTeapApp.java
@@ -10,8 +10,6 @@ public class LaunchTeapApp {
 	public static WebDriver LaunchApp()
 	{
 		String driverpath = Paths.get("src", "test", "resources","webdrivers","chromedriver.exe").toFile().getAbsolutePath();
-		//System.out.println(driverpath);
-		//System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\drivers\\chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", driverpath);
 		WebDriver driver = new ChromeDriver();
 		driver.get(Settings.URL);
--- src/test/java/teap/testsuite/Testcase_CasecsRegression.java
+++ src/test/java/teap/testsuite/Testcase_CasecsRegression.java
@@ -1,15 +1,16 @@
 package teap.testsuite;
 
 import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectClasses;
-//import org.junit.platform.suite.api.SelectPackages;
+//import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses( teap.tests.Testcase_POC.class )
+@SelectPackages("demo.tests")
+//@SelectClasses( demo.tests.DemoTestcase.class )
 //@SelectClasses({teap.tests.Testcase_POC.class, teap.tests.Testcase2.class } )
 //@SelectPackages({"Test1","teap.tests"})
-//@SelectPackages("Test1")
+//@SelectPackages("Test1");
 public class Testcase_CasecsRegression {
 
 }
